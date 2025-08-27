9f443f80c342d3bce894bd0937c9bb1c7bde3de3
Talha Tahir <talhabintahir2001@gmail.com>
2025-08-27 22:45:03 +0500

    Third Commit

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,7 @@
 import { Canvas, useFrame } from "@react-three/fiber";
-import HeroText from "../components/HeroText";
-import ParallaxBackground from "../components/ParallaxBackground";
-import { Astronaut } from "../components/Astronaut";
+import HeroText from "../components/HeroText.jsx";
+import ParallaxBackground from "../components/parallaxBackground.jsx";
+import { Astronaut } from "../components/Astronaut.jsx";
 import { Float } from "@react-three/drei";
 import { useMediaQuery } from "react-responsive";
 import { easing } from "maath";
