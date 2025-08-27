20348567c9e175fa5f7739f2642a32cd0e69c3e3
Talha Tahir <talhabintahir2001@gmail.com>
2025-08-28 00:27:29 +0500

    Fifth Commit

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -61,7 +61,7 @@ const HeroText = () => {
           animate="visible"
           transition={{ delay: 1 }}
         >
-          Hi,I'm Ali
+          Hi,I'm Talha Tahir
         </motion.p>
         <div>
           <motion.p
